15751eb11584404d468395c0e253c5823c95f9c9
gdCorderPlay <845778664@qq.com>
2018-03-16 18:52:00 +0800

    归并 递归

---
+++ DataTest/Assets/MergeSortTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 归并排序测试
+/// </summary>
+public class MergeSortTest  {
+
+
+
+
+    /// <summary>
+    ///  
+    /// </summary>
+    void Test(int[] test ,int count)
+    {
+
+
+
+    }
+    void  test2(int[] test,int[] temp,int left,int index,int right)
+    {
+        int leftIndex = left;
+        int rightIndex = index;
+        int insert = left;
+
+        while (insert <= right)
+        {
+
+            if (test[leftIndex] > test[rightIndex])
+            {
+                temp[insert] = test[rightIndex];
+                rightIndex++;
+            }else
+            {
+                temp[insert] = test[leftIndex];
+                leftIndex++;
+            }
+            insert++;
+        }
+    }
+
+
+
+
+
+
+	
+}
