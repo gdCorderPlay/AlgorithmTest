82fb5ccd361b4f76802dbb78c222d94611682425
gdCorderPlay <845778664@qq.com>
2018-03-17 14:29:25 +0800

    快速排序

--- DataTest/Assets/MergeSortTest.cs
+++ DataTest/Assets/MergeSortTest.cs
@@ -5,46 +5,86 @@ using UnityEngine;
 /// <summary>
 /// 归并排序测试
 /// </summary>
-public class MergeSortTest  {
+public class MergeSortTest :MonoBehaviour  {
 
+    public int[] nums;
 
-
-
+    private void Start()
+    {
+        MergeSort(nums, nums.Length);
+        
+    }
     /// <summary>
-    ///  
+    ///  供外部调用的方法
     /// </summary>
-    void Test(int[] test ,int count)
+    void MergeSort(int[] test ,int count)
     {
 
+        int[] temp = new int[count];
+
+        DiGui(test, temp, 0, (count - 1) / 2, count - 1);
 
 
     }
+    /// <summary>
+    /// 主体的递归程序
+    /// </summary>
+    
+    void DiGui(int[] test, int[] temp,int left, int index, int right)
+    {
+        if(left<index)
+        DiGui(test,temp,left,(left+index)/2,index);  //左边部分
+        if(index+1<right)
+        DiGui(test, temp, index+1, (right + index+1) / 2, right); //右边部分
+        //归并
+        test2(test, temp, left, index, right);
+
+
+    }
+    /// <summary>
+    /// 归并两个有序的数组
+    /// </summary>
     void  test2(int[] test,int[] temp,int left,int index,int right)
     {
         int leftIndex = left;
-        int rightIndex = index;
+        int rightIndex = index+1;
         int insert = left;
-
         while (insert <= right)
         {
-
-            if (test[leftIndex] > test[rightIndex])
+            if (leftIndex <= index && rightIndex <= right)
             {
-                temp[insert] = test[rightIndex];
-                rightIndex++;
-            }else
+                if (test[leftIndex] > test[rightIndex])
+                {
+                    temp[insert] = test[rightIndex];
+                    rightIndex++;
+                }
+                else
+                {
+                    temp[insert] = test[leftIndex];
+                    leftIndex++;
+                }
+            }
+            else
             {
-                temp[insert] = test[leftIndex];
-                leftIndex++;
+                if(leftIndex <= index)
+                {
+                    temp[insert] = test[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    temp[insert] = test[rightIndex];
+                    rightIndex++;
+                }
             }
             insert++;
         }
+        for (; left <= right; left++)
+        {
+            test[left] = temp[left];
+        }
+
     }
-
-
-
-
-
 
 	
 }
---
+++ DataTest/Assets/QuickSortTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// 快速排序练习
+/// </summary>
+public class QuickSortTest : MonoBehaviour {
+
+    public int[] nums;
+
+    private void Start()
+    {
+        Sort(nums, 0, nums.Length - 1);
+    }
+    void Sort(int[]nums,int start,int end)
+    {
+        if (start >= end)
+        {
+            return;
+        }
+        int left = start;
+        int right = end;
+        int temp;
+        while (left < right)
+        {
+           
+            while (nums[left]<nums[end]&& left <= right)
+            {
+                left++;
+            }
+            while (nums[right] >=nums[end]&& left <= right)
+            {
+                right--;
+            }
+            //交换位置
+            if (left < right)
+            {
+                temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+            
+        }
+        temp = nums[end];
+        nums[end] = nums[left];
+        nums[left ] = temp;
+
+        Sort(nums, start,left-1);//左边部分
+        Sort(nums, left+1,end);//右边部分
+
+    }
+	
+
+
+}
